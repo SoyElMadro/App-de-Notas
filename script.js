f3e16fb522b83c8d3798b4dacd1f02951bc25ee1
SoyElMadro <gonzalosubs@gmail.com>
2023-06-30 20:46:03 -0300

    add: notes saved on localStorage

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const noteTitle = document.getElementById("note-title");
 const noteInput = document.getElementById("note-input");
 const notesContainer = document.getElementById("notes-container");
 
+let notes = [];
+
+if (localStorage.getItem("notes")) {
+  notes = JSON.parse(localStorage.getItem("notes"));
+  restoreNotes();
+}
+
 createBtn.addEventListener("click", createNote);
 
 function createNote() {
@@ -11,12 +18,28 @@ function createNote() {
   const noteText = noteInput.value.trim();
   
   if (noteTitleText !== "" && noteText !== "") {
-    const note = document.createElement("div");
-    note.className = "note";
-    note.style.backgroundColor = colorPicker.value;
+    const note = {
+      title: noteTitleText,
+      content: noteText,
+      color: colorPicker.value,
+    };
+
+    notes.push(note);
+    saveNotes();
+    addNoteToContainer(note);
+
+    noteInput.value = "";
+    colorPicker.value = "#ffffff";
+  }
+}
+
+function addNoteToContainer(note) {
+  const noteElement = document.createElement("div");
+    noteElement.className = "note";
+    noteElement.style.backgroundColor = colorPicker.value;
     
     const noteContent = document.createElement("p");
-    noteContent.textContent = noteText;
+    noteContent.textContent = note.content;
     
     const noteHeader = document.createElement("div");
     noteHeader.className = "note-header";
@@ -25,19 +48,19 @@ function createNote() {
     noteFooter.className = "note-footer";
     
     const titleElement = document.createElement("h3");
-    titleElement.textContent = noteTitleText;
+    titleElement.textContent = note.title;
 
     const editNoteIcon = document.createElement("i");
     editNoteIcon.classList.add('bx', 'bxs-edit');
     editNoteIcon.addEventListener("click", () => {
-      if (note.classList.contains("editing")) {
-        note.classList.remove("editing");
+      if (noteElement.classList.contains("editing")) {
+        noteElement.classList.remove("editing");
         editNoteIcon.classList.remove("bx-x-circle");
         editNoteIcon.classList.add("bxs-edit");
         titleElement.contentEditable = false;
         noteContent.contentEditable = false;
       } else {
-        note.classList.add("editing");
+        noteElement.classList.add("editing");
         editNoteIcon.classList.remove("bxs-edit");
         editNoteIcon.classList.add("bx-x-circle");
         titleElement.contentEditable = true;
@@ -57,20 +80,26 @@ function createNote() {
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Eliminar";
     deleteBtn.addEventListener("click", () => {
-      note.remove();
+      noteElement.remove();
     });
     
     noteHeader.appendChild(titleElement);
     noteHeader.appendChild(editNoteIcon);
     noteFooter.appendChild(deleteBtn);
     
-    note.appendChild(noteHeader);
-    note.appendChild(noteContent);
-    note.appendChild(noteFooter);
+    noteElement.appendChild(noteHeader);
+    noteElement.appendChild(noteContent);
+    noteElement.appendChild(noteFooter);
     
-    notesContainer.appendChild(note);
-    
-    noteInput.value = "";
-    colorPicker.value = "#ffffff";
-  }
+    notesContainer.appendChild(noteElement);
+}
+
+function saveNotes() {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+function restoreNotes() {
+  notes.forEach((note) => {
+    addNoteToContainer(note);
+  });
 }
